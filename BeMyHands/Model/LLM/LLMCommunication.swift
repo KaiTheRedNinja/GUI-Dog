75274ebbb422e7abc3dfef76b60de21d28105f33
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2024-07-16 09:54:08 +0800

    Split up files

---
+++ BeMyHands/Model/LLM/LLMCommunication.swift
@@ -0,0 +1,75 @@
+//
+//  LLMCommunication.swift
+//  BeMyHands
+//
+//  Created by Kai Quan Tay on 16/7/24.
+//
+
+import Element
+
+/// Holds and manages the data associated with the communication between BeMyHands and the LLM.
+///
+/// Acts as the interface for LLMs to take actions.
+class LLMCommunication {
+    /// The current map of elements
+    private(set) var elementMap: [String: ActionableElement]
+
+    /// The current step's context. Nil if no steps have been taken.
+    private(set) var stepContext: ActionStepContext?
+
+    init(elementMap: [String: ActionableElement]) {
+        self.elementMap = elementMap
+        self.stepContext = nil
+    }
+
+    /// Creates a `stepContext` starting at step zero for steps. Should only be called ONCE, and will
+    /// fatal error if called multiple times on the same instance.
+    func setup(withSteps steps: [String]) {
+        assert(stepContext == nil, "Step context must not exist during setup stage")
+        stepContext = .init(allSteps: steps, currentStep: 0)
+    }
+
+    /// Updates the elementMap
+    func updateCurrentElements(to elementMap: [String: ActionableElement]) {
+        self.elementMap = elementMap
+    }
+
+    /// Executes an action on an element with a certain description. Handles data checks to make sure that the element
+    /// exists and that the action is valid.
+    func execute(action: String, onElementWithDescription elementDescription: String) async throws {
+        guard action.hasPrefix("AX") && !action.contains(" ") else {
+            throw LLMCommunicationError.actionFormatInvalid
+        }
+
+        guard let element = elementMap[elementDescription] else {
+            throw LLMCommunicationError.elementNotFound
+        }
+
+        guard element.actions.contains(action) else {
+            throw LLMCommunicationError.actionNotFound
+        }
+
+        try await element.element.performAction(action)
+    }
+}
+
+/// An error in LLM communication
+enum LLMCommunicationError: Error {
+    /// The action is not formatted properly
+    case actionFormatInvalid
+
+    /// Element was not found in the ``LLMCommunication`` instance
+    case elementNotFound
+    /// Action is not a valid action on the `Element` instance
+    case actionNotFound
+}
+
+/// A structure that holds the context of a single "step" in Phase 2
+struct ActionStepContext {
+    /// The full list of steps
+    var allSteps: [String]
+    /// The current step
+    var currentStep: Int
+    /// The past actions done in this step. Nil if this is the first sub-step.
+    var pastActions: [String]?
+}
