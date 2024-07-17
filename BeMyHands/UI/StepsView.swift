d4c5433b7da35730f840ce97bdf3ad6ca6f9d331
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2024-07-17 13:00:31 +0800

    Upgrade steps view

--- BeMyHands/UI/StepsView.swift
+++ BeMyHands/UI/StepsView.swift
@@ -16,26 +16,13 @@ struct StepsView: View {
             Spacer()
             if let stepContext {
                 List {
-                    ForEach(Array(stepContext.allSteps.enumerated()), id: \.offset) { (index, step) in
-                        HStack {
-                            Group {
-                                if index == stepContext.currentStep {
-                                    ProgressView()
-                                        .progressViewStyle(.circular)
-                                } else {
-                                    let isBefore = index < stepContext.currentStep
-                                    Image(systemName: isBefore ? "checkmark.circle" : "circle")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .foregroundStyle(isBefore ? Color.green : .gray)
-                                }
-                            }
-                            .frame(width: 30, height: 30)
+                    Section {
+                        Text(stepContext.goal)
+                            .font(.title)
+                    }
 
-                            Text(step)
-
-                            Spacer()
-                        }
+                    Section("Steps") {
+                        stepsSection(stepContext: stepContext)
                     }
                 }
                 .frame(height: size.width)
@@ -46,4 +33,53 @@ struct StepsView: View {
         .frame(width: size.width, height: size.height)
         .animation(.default, value: stepContext)
     }
+
+    @ViewBuilder
+    func stepsSection(stepContext: ActionStepContext) -> some View {
+        if !stepContext.allSteps.isEmpty {
+            ForEach(Array(stepContext.allSteps.enumerated()), id: \.offset) { (index, step) in
+                HStack {
+                    Group {
+                        if index == stepContext.currentStep {
+                            ProgressView()
+                                .progressViewStyle(.circular)
+                        } else {
+                            let isBefore = index < stepContext.currentStep
+                            Image(systemName: isBefore ? "checkmark.circle" : "circle")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundStyle(isBefore ? Color.green : .gray)
+                        }
+                    }
+                    .frame(width: 30, height: 30)
+
+                    Text(step)
+
+                    Spacer()
+                }
+            }
+        } else {
+            Text("Loading...")
+        }
+    }
+}
+
+#Preview {
+    StepsView(
+        stepContext: .init(
+            goal: "Random Goal",
+            allSteps: (0..<5).map { "Step \($0)" },
+            currentStep: 2
+        ),
+        size: .init(width: 373, height: 373)
+    )
+
+    StepsView(
+        stepContext: .init(
+            goal: "Random Goal",
+            allSteps: [],
+            currentStep: 2
+        ),
+        size: .init(width: 373, height: 373)
+    )
 }
